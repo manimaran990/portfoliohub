724ee8dde09aa7b020b7b376f2c7d2d48d807323
m4n1g <manimaran990@gmail.com>
2021-04-24 12:30:42 -0400

    added cors policy

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from portfolio import MyPortfolio
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 mp = MyPortfolio()
 
 @app.get("/getgoldrate")
