efe1cce19f855d30a372a4a33809c36ea035cfed
m4n1g <manimaran990@gmail.com>
2021-04-29 16:00:51 -0400

    changes in mf portfolio

--- portfolio.py
+++ portfolio.py
@@ -6,19 +6,25 @@ import pprint
 import json
 import math
 import requests
-import pandas as pd
 from datetime import datetime, timedelta
 from functools import reduce
 import pytz
+import copy
+import math
+import re
 from bs4 import BeautifulSoup
 from fastapi.responses import HTMLResponse
 
 
 class MyPortfolio(object):
 	def __init__(self):
+		today = datetime.today()
+		curr_date = today.strftime("%d-%b-%Y")
+		prev_date = (today - timedelta(1)).strftime("%d-%b-%Y")
 		self.xch_url = "https://v6.exchangerate-api.com/v6/931fba7839f1267eba9ede7f/latest/CAD" #url for cad to inr rate
-		self.mf_url = "https://api.mfapi.in/mf/" #mutual fund api
-		self.bt_url = "https://blockchain.info/ticker" #bitcoin api
+		self.curr_mf_url = "https://www.amfiindia.com/spages/NAVAll.txt"
+		self.prev_mf_url = f"https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?tp=1&frmdt={prev_date}&todt={prev_date}"
+		self.bt_url = "https://api.blockchain.com/v3/exchange/tickers/" #bitcoin api
 		self.mf_ids = { 
 		   "nippon_liquid_fund": 118701,
 		   "axis_midcap": 120505,
@@ -28,8 +34,11 @@ class MyPortfolio(object):
 		   "mo_nasdaq_fof": 145552
 		}
 		self.headers = {
-		    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0",
+			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0",
 		}
+
+	def dict_subtract(self, dict1, dict2):
+		return { k: round(v - dict2[k], 2)  for k,v in dict1.items() }
 
 	#to get current gold rate in inr
 	def get_goldrate(self):
@@ -63,59 +72,71 @@ class MyPortfolio(object):
 			return {"success": False, "error": str(e)}
 		return curr_exch_rate
 
-	def _get_mf_df(self, mfid, days):
+	def _get_mf_df(self):
 		try:
-			url = f"https://api.mfapi.in/mf/{mfid}"
-			resp = requests.get(url)
-			df = pd.DataFrame.from_dict(resp.json()["data"])
-			as_types = {'date': 'datetime64[ns]', "nav": float}
-			df = df.astype(as_types)
-			df = df.rename(columns = {'nav': f'{mfid}'})
-			
-			last_5 = datetime.now() - timedelta(days=days)
-			current_date = datetime.now().strftime("%Y-%m-%d")
-			last_5_date = last_5.strftime("%Y-%m-%d")
+			curr = requests.get(self.curr_mf_url).text
+			prev = requests.get(self.prev_mf_url).text
 
-			filtered_df = df.loc[(df['date'] > last_5_date) & (df['date'] < current_date)]
+			#reverse mf_ids dictionary
+			mf_names = { str(v): k for k, v in self.mf_ids.items() }
+
+			#current mf nav changes
+			curr_lst = []
+			for k,v in self.mf_ids.items():
+				line = re.search(f'{v}.*', curr).group(0)
+				lst = line.strip().split(';')
+				date = {lst[5]}
+				curr_lst.append( (lst[0], float(lst[4]) ) )
+
+			#previous mf nav changs
+			prev_lst = [] 
+			for k,v in self.mf_ids.items():
+				line = re.search(f'{v}.*', prev).group(0)
+				lst = line.strip().split(';')
+				date = {lst[7]}
+				prev_lst.append( (lst[0], float(lst[4]) ) )
+
+			curr_dict = dict(curr_lst)
+			prev_dict = dict(prev_lst)
+			diff_dict = self.dict_subtract(curr_dict, prev_dict)
+
+			keys = diff_dict.keys()
+
+			fin_dict = {}
+			for k in keys:
+				fin_dict[k] = { "name": mf_names[k],"current" : curr_dict[k], "prev": prev_dict[k], "diff": diff_dict[k] }
 		except Exception as e:
-			return None
-		return filtered_df
+			return str(e)
+		return fin_dict
 
 	#mutual funds
 	def get_mf_nav_rates(self):
 		try:
-			mf_ids2 = {y:x for x,y in self.mf_ids.items()}
-			frames = []
-			for k, v in self.mf_ids.items():
-				frames.append(self._get_mf_df(v, 5))
-
-			fin_df = reduce(lambda x, y: pd.merge(x, y, on = 'date'), frames)
-			fin_df1 = fin_df.set_index('date').T
-			out = fin_df1.to_json(date_format = 'iso')
-			out = reduce(lambda x, y: x.replace(str(y), mf_ids2[y]), mf_ids2, out)
-			mf_data = json.loads(out)
-
-			key_list = sorted(mf_data.keys(), reverse=True)
-			val_list = []
-			for i in range(2): #pick first two
-				val_list.append(mf_data[key_list[i]])
-
-			nav_diffs = { k: round(v - val_list[1][k],3) for k, v in val_list[0].items() }
+			mf_changes = self._get_mf_df()
 		except Exception as e:
 			return {"success": False, "error": str(e)}
-		return { "success": True, "mf_portfolio": { key_list[0]:nav_diffs} }
+		return { "success": True, "mf_portfolio": mf_changes }
 
 	#bitcoin to cad
 	def get_bitcoin_rates(self):
-		resp = requests.get(self.bt_url)
-		fifteen_min = resp.json()["CAD"]["15m"]
-		last = resp.json()["CAD"]["last"]
-		return { "bitcoin": {"fifteen_min": fifteen_min, "last": last}}
+		tickers = ['ETH-USD', 'BTC-USD']
+		keys = ['symbol', 'last_trade_price']
+		crypt_data = []
+		for ticker in tickers:
+			resp = requests.get(self.bt_url+ticker).json()
+			crypt_data.append(tuple([ resp[k] for k in keys ]))
+
+		crypt_usd = dict(tuple([ tuple(k) for k in crypt_data ]))
+		crypt_cad = copy.deepcopy(crypt_usd)
+		for k in tickers:
+			crypt_cad[k] = math.floor(crypt_usd[k] * requests.get("https://v6.exchangerate-api.com/v6/931fba7839f1267eba9ede7f/latest/USD").json()['conversion_rates']['CAD'])
+
+		return { "success": True,  "cyptos":  crypt_cad}
 
 
 if __name__ == '__main__':
 	mp = MyPortfolio()
-	print(mp.get_goldrate())
+	# print(mp.get_goldrate())
 	# print(mp.get_currencyrate())
-	# print(mp.get_mf_nav_rates())
+	print(mp.get_mf_nav_rates())
 	# print(mp.get_bitcoin_rates())
